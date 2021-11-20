f5bc816a6da9886a4d5be00ddb7f8a1687e30355
AstudilloDante <93348608+AstudilloDante@users.noreply.github.com>
2021-11-19 22:09:35 -0500

    Update Usuario.java

--- src/main/java/Sistema/Usuario.java
+++ src/main/java/Sistema/Usuario.java
@@ -10,5 +10,32 @@ package Sistema;
  * @author User
  */
 public class Usuario {
+    protected String Ususario;
+    protected String Contraseña;
+    
+    protected String identificarUsusario(String Ususario,String Contraseña){
+        ManejoArchivos usuarioC=new ManejoArchivos();
+        Arraylist usuarioCl = usuarioC.LeeFichero(clientes.txt);
+
+        for (i=0;i<usuarioCl.length;i++){
+            String user =usuarioCl[i][3];
+            String contra=usuarioCl[i][4];
+            String tipoDU=ususarioCl[i][6];    
+            if (Usuario.equals(User) and Contraseña.equals(contra)
+                return tipoDU;
+                break
+    
+         
+        
+            
+                
+        }
+    }        
+            
+    
+    
+        
+    
+    
     
 }
