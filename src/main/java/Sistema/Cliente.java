155ec765b71d67dc9fef6db909b629909899e394
AstudilloDante <93348608+AstudilloDante@users.noreply.github.com>
2021-11-19 23:38:39 -0500

    Update Cliente.java

--- src/main/java/Sistema/Cliente.java
+++ src/main/java/Sistema/Cliente.java
@@ -12,19 +12,33 @@ import java.util.ArrayList;
  */
 public class Cliente extends Usuario{
     private int numtarjetaCredito;
+    private String cedula;
+    
     protected ArrayList ServiciosPedidos;
+    
+    private Cliente(){
+        manejoArchivos cliente=new manejoArchivos();
+        Arraylist<String> cliente.LeeFichero(cliente.txt);
+         for (i=0;i<usuarioCl.length;i++){
+            this. =usuarioCl[i][3];
+            String contra=usuarioCl[i].counte;
+            String tipoDU=ususarioCl[i][6];    
+            
+    
+    }
+    
     protected void solicitarDelivery (){
         
-    Delivery serviciodeilivery = new Delivery();
+        Delivery serviciodeilivery = new Delivery();
     }
     protected void consultarServicios(){
     //aqui va lo de consultar servicio
     }
     protected void solicitarTaxi(){
-    Taxi serviciotaxi= new Taxi();
+        Taxi serviciotaxi= new Taxi();
     }
     protected void Encomientda(){
-    Encomienda servicioencomienda =new Encomienda();
+        Encomienda servicioencomienda =new Encomienda();
         
     }
 }
