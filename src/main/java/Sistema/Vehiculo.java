c2e5386c20e4344539b3fcfdfe51a1b1f18a9d07
Chantalex14 <93546192+Chantalex14@users.noreply.github.com>
2021-11-19 22:59:02 -0500

    lectura vehiculos

--- src/main/java/Sistema/Vehiculo.java
+++ src/main/java/Sistema/Vehiculo.java
@@ -10,22 +10,48 @@ package Sistema;
  * @author User
  */
 public class Vehiculo {
-    private String tipoVehiculo;
-    private String placa;
-    private String marca;
-    private String modelo;
     private int codigoVehiculo;
+    private String placa;
+    private String modelo;
+    private String marca;    
+    private String tipoVehiculo;
     
     //leer archivo vehiculos.txt
     //codigoVehiculo, placa, modelo, marca, tipoVehiculo
     
     private Vehiculo(){
-        this.codigoVehiculo = ;
-        this.placa = ;
-        this.marca = ;
-        this.modelo = ;
-        this.tipoVehiculo = ;
+        ManejoArchivos vehiculos = new ManejoArchivos();
+        Arraylist vehiculos1 = vehiculos.LeeFichero("vehiculos.txt");
+        for (i=0;i<vehiculos1.length;i++){
+            this.codigoVehiculo = vehiculos1[i][0];
+            this.placa = vehiculos1[i][1];
+            this.modelo = vehiculos1[i][2];
+            this.marca = vehiculos1[i][3];
+            this.tipoVehiculo = vehiculos1[i][4]
+        }
+        
+        //this.codigoVehiculo = ;
+        //this.placa = ;
+        //this.maodelo = ;
+        //this.marca = ;
+        //this.tipoVehiculo = ;
         
     }
-        
+    private Vehiculo(int codV){
+        ManejoArchivos vehiculos = new ManejoArchivos();
+        Arraylist vehiculos1 = vehiculos.LeeFichero("vehiculos.txt");
+        for (i=0;i<vehiculos1.length;i++){
+            int codV1 = Integer.valueOf(vehiculos1[i][1]);
+            String placa1 = vehiculos1[i][1];
+            String modelo1 = vehiculos1[i][2];
+            String marca1 = vehiculos1[i][3];
+            String tipoVehiculo1 = vehiculos1[i][4]
+            if (codV.equals(codV1){
+                this.placa = placa1;
+                this.modelo = modelo1;
+                this.marca = marca1;
+                this.tipoVehiculo = tipoVehiculo1;
+            }
+        }
+    }
 }
