1f7e55f822a2fb11ec2e13df4c595a0c0d453310
Chantalex14 <93546192+Chantalex14@users.noreply.github.com>
2021-11-20 22:21:43 -0500

    constructor taxi

--- src/main/java/Sistema/Taxi.java
+++ src/main/java/Sistema/Taxi.java
@@ -12,8 +12,9 @@ package Sistema;
 public class Taxi extends Servicio {
     private int numeroPasajeros;
     
-    
-    
-    
+    public Taxi(tipoServicio tipo, Date fecha, String horadePartida, boolean aceptarServicio, int codigo, int numeroPasajeros) {
+        super(tipo, fecha, horadePartida, aceptarServicio, codigo);
+        this.numeroPasajeros = numeroPasajeros;
+    }    
     
 }
