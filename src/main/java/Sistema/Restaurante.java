0de4ba3c97b7d5f9a87e801b3d7a433ea8ef8cf3
AstudilloDante <93348608+AstudilloDante@users.noreply.github.com>
2021-11-26 21:27:08 -0500

    Update Restaurante.java

--- src/main/java/Sistema/Restaurante.java
+++ src/main/java/Sistema/Restaurante.java
@@ -13,19 +13,24 @@ import java.util.ArrayList;
 public class Restaurante {
     
     
-    ArrayList listadoPlatosDisponibles= new ArrayList();
-    
-    private int listadeplatos(int codigo){
-        ArrayList<String> listaplatos= ManejoArchivos.LeerInformacion("menus.txt");
-        int enumerar=0;
+    ArrayList<String> listadoPlatosDisponibles= new ArrayList();
+    public Restaurante(int codigo){
+        
         for (int i=0;i<listaPlatos.size();i++){
             if (codigo.equals(listaPlatos.get(i).split(",")[0])){                
-                System.out.println(enumerar +".- "+ listaPlatos.get(i).split(",")[1]);
-                enumerar++
+               listadoPlatosDisponibles.add(listaPlatos.get(i).split(",")[1])
+                
             }
             
         
         }
+    
+    }
+    
+    private int listadeplatos(int codigo){
+        ArrayList<String> listaplatos= ManejoArchivos.LeerInformacion("menus.txt");
+        
+        
     
     }
     
