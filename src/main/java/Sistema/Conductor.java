f6d0709494a562aafd1351fd5cf310b9eb8254c8
melpgala <93047039+melpgala@users.noreply.github.com>
2021-11-26 19:18:06 -0500

    Update Conductor.java

--- src/main/java/Sistema/Conductor.java
+++ src/main/java/Sistema/Conductor.java
@@ -13,10 +13,9 @@ import manejoArchivos.ManejoArchivos;
  * @author User
  */
 public class Conductor extends Usuario{
-    //private String user;
     private String nombre; //se bucan en usuarios.txt a partir de user
     private String apellido; //
-    private int numCelular; //
+    private String numCelular; //
     private String estado;
     private int licencia;
     private int codigoVeh;
@@ -24,15 +23,14 @@ public class Conductor extends Usuario{
     
     
     //leer archivo conductores.txt
-    //cedula, licencia, estado, codigoVehiculo    
+    //user, licencia, estado, codigoVehiculo    
     private Conductor(String cedula){        
-        super();
-       
+
         ArrayList <String> conductores1 = ManejoArchivos.LeeFichero("conductores.txt");
-        String userCon="";
+        String cedConductor="";
         for (int i=0;i<conductores1.size();i++){
-            cedCon = conductores1.get(i).split(",")[0];
-            if(cedCon.equals(cedula)){
+            cedConductor = conductores1.get(i).split(",")[0];
+            if(cedConductor.equals(cedula)){
                 this.licencia = Integer.valueOf(conductores1.get(i).split(",")[1]);
                 this.estado = conductores1.get(i).split(",")[2];
                 codigoVeh = Integer.valueOf(conductores1.get(i).split(",")[3]);
@@ -46,43 +44,32 @@ public class Conductor extends Usuario{
             String nom = users1.get(i).split(",")[1];
             String apell = users1.get(i).split(",")[2];
             String ced1 = users1.get(i).split(",")[0];
-            int numCel = Integer.valueOf(users1.get(i).split(",")[5]);
-            if(ced1.equals(cedCon)){
+            String numCel = users1.get(i).split(",")[5];
+            if(ced1.equals(cedConductor)){
                 this.nombre = nom;
                 this.apellido = apell;
                 this.numCelular = numCel;
             }
         }
-        
-        this.veh = new Vehiculo(codigoVeh);
-        //this.user = ;
-        //this.licencia = ;
-        //this.estado = ;
-        //this.codigoVehiculo = ;
-        
+        this.veh = new Vehiculo(codigoVeh); 
     }
+    
     public String getNombre(){
         return nombre;
     }
     public String getApellido(){
         return apellido;
     }
-    public int getNumCelular(){
+    public String getNumCelular(){
         return numCelular;
     }
-    //private String getUser(){
-        //return user;
-    //}
+    
     public String getEstado(){
         return estado;
     }
     public int getLicencia(){
         return licencia;
     }
-    //private int getCodigoVehiculo(){
-        //return codigoVehiculo;
-    //}
-    
 
     
     @Override
