48b42eced4d9cb7786dc3162a7799fa0ede841ef
melpgala <93047039+melpgala@users.noreply.github.com>
2021-11-28 12:51:48 -0500

    comentarios

--- src/main/java/Sistema/Taxi.java
+++ src/main/java/Sistema/Taxi.java
@@ -1,28 +1,36 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sistema contiene las clases del proyecto.
  */
 package Sistema;
-
 import Enums.tipoServicio;
 import java.time.LocalDateTime;
 
 
 /**
- *
- * @author User
+ * Esta clase contiene los métodos y atributos de un taxi.
+ * @author Melanie Peñaherrera
+ * @see Servicio
  */
 public class Taxi extends Servicio {
     private int numeroPasajeros;
     
+    /**
+    * Método que devuelve el número de pasajeros que tomaran el taxi.
+    * @return Devuelve un int que indica el número de pasajeros que tomaran el taxi.
+    */
     protected int getNumeroPasajeros(){
         return numeroPasajeros;
     }
     
+    /**
+    * Método constructor parametrizado de la clase Taxi.
+    * @param tipo Tipo de servicio.
+    * @param fechaHora Fecha y hora en la que se pidio el servicio.
+    * @param codigo Código por el servicio.
+    * @param numeroPasajeros Número de pasajeros que tomaran el taxi.
+    */
     public Taxi(tipoServicio tipo, LocalDateTime fechaHora, int codigo, int numeroPasajeros) {
         super(tipo, fechaHora, codigo);
         this.numeroPasajeros = numeroPasajeros;
-    }    
-    
+    }      
 }
