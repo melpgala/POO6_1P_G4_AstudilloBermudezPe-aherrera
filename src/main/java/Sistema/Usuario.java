d4e827835108e0e344f4b4836bc10a2837d60a5f
melpgala <93047039+melpgala@users.noreply.github.com>
2021-11-28 12:58:49 -0500

    comentarios

--- src/main/java/Sistema/Usuario.java
+++ src/main/java/Sistema/Usuario.java
@@ -1,14 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sistema contiene las clases del proyecto.
  */
 package Sistema;
 import java.util.ArrayList;
 import manejoArchivos.ManejoArchivos;
 
 /**
- *
+ * Esta clase es abstracta y contiene los métodos y atributos de un usuario.
  * @author melpgala
  * @author AstudilloDante
  */
@@ -16,47 +14,38 @@ public abstract class Usuario {
     protected String Usuario;
     protected String Contrasenia;
     
-   
-    
-
-//        ArrayList <String> usuarioCl = ManejoArchivos.LeeFichero("Usuario.txt");
-//
-//        for (int i=0;i<usuarioCl.size();i++){
-//            this.Usuarios =usuarioCl.get(i).split(",")[3];
-//            this.Contrasenia =usuarioCl.get(i).split(",")[4];
-//        }
-    
+    /**
+    * Método que devuelve el usuario.
+    * @return Devuelve un String que indica el usuario.
+    */
     protected String getUsuarios(){
         return Usuario;
     }
-    
-    protected String getContrasenia(){
-        return Contrasenia;
-    }
-    /**
-    *@return {String} tipoDU1, returno el tipo de usuario "C" cliente y "R" conductor
-    *@param{String} Usuarios, el nickname del usuario 
-    *@param{String} Contrasenia, contraseña del usuario
-    *
-    */
-    
-    protected String identificarUsuario(String Usuarios,String Contrasenia){
+   
  
+    /**
+    * Método que identifica que tipo de usuario es.
+    * @param Usuarios Nombre de usuario 
+    * @param Contrasenia Contraseña del usuario
+    * @return Devuelve el tipo de usuario; "C" cliente y "R" conductor.
+    */
+    protected String identificarUsuario(String Usuarios,String Contrasenia){
         ArrayList <String> usuarioCl = ManejoArchivos.LeeFichero("Usuario.txt");
-        
         String tipoDU1="";
         for (int i=0;i<usuarioCl.size();i++){
             String user =usuarioCl.get(i).split(",")[3];
             String contra=usuarioCl.get(i).split(",")[4];
             String tipoDU=usuarioCl.get(i).split(",")[6];    
             if (Usuarios.equals(user) && Contrasenia.equals(contra)){
-                
                 tipoDU1=tipoDU;
-                
             }
         }
         return tipoDU1;
     }
+    
+    /**
+    * Método abstracto para consultar un servicio.
+    */
     protected abstract void consultarServicio();
 
 }
