6d9a269cc9298c9483e9516fb432fd0e00b60ce7
melpgala <93047039+melpgala@users.noreply.github.com>
2021-11-28 12:28:23 -0500

    comentarios

--- src/main/java/Sistema/Ruta.java
+++ src/main/java/Sistema/Ruta.java
@@ -1,31 +1,44 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sistema contiene las clases del proyecto.
  */
 package Sistema;
+import java.util.Scanner;
 
 /**
- *
- * @author User
+ * Esta clase contiene los métodos y atributos de una ruta.
+ * @author Melanie Peñaherrera
  */
-import java.util.Scanner;
 public class Ruta {
     private String origen;
     private String destino;
     private Scanner sc;
-    protected Ruta(String origen, String destino){
-        this.origen=origen;
-        this.destino=destino;
     
-    }
-    
+    /**
+    * Método que devuelve el origen de la ruta.
+    * @return Devuelve un String que indica el origen de la ruta.
+    */
     protected String getOrigen(){
         return origen;
     }
     
+    /**
+    * Método que devuelve el destino de la ruta.
+    * @return Devuelve un String que indica el destino de la ruta.
+    */
     protected String getDestino(){
         return destino;
+    }
+    
+    /**
+    * Método constructor de la clase Ruta.
+    * @author Melanie Peñeherrera
+    */
+    protected Ruta(){
+        sc = new Scanner(System.in);
+        System.out.println("Ingrese origen: ");
+        origen = sc.nextLine();
+        System.out.println("Ingrese destino: ");
+        destino = sc.nextLine();
     }
     
 }
