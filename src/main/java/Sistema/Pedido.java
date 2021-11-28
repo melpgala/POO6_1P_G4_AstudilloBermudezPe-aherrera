e5bb69fdf64c07869df0b19fe6caa577594672bd
melpgala <93047039+melpgala@users.noreply.github.com>
2021-11-28 12:01:40 -0500

    comentarios

--- src/main/java/Sistema/Pedido.java
+++ src/main/java/Sistema/Pedido.java
@@ -1,46 +1,42 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sistema contiene las clases del proyecto.
  */
 package Sistema;
 import java.util.ArrayList;
 
 /**
- *
- * @author melpgala
- * @Astudillo Dante
+ * Esta clase contiene los métodos y atributos de un pedido.
+ * @author Melanie Peñaherrera
+ * @author Dante Astudillo
  */
 public class Pedido {
     private String nombreRestaurante;
-    private ArrayList  <Plato> platosElegidos  = new ArrayList();// {}
+    private ArrayList  <Plato> platosElegidos  = new ArrayList();
+    
     /**
-    *Metodo que devuelve el atributo ArrayList<Plato>
-    *@return {ArrayList<plato>} platosElegidos devuelve un Arraylist<Plato>
+    * Método que devuelve un ArrayList de platos.
+    * @return Devuelve un Arraylist de Platos.
     */
-     
     public ArrayList <Plato> getPlatosElegidos(){
         return platosElegidos;
     }
+    
     /**
-    *@return {String} pedido, devuelve un String del nombre de todos los platos escojidos por el usuario
-    *
+    * Método que devuelve los nombres de los platos del pedido.
+    * @return Devuelve un String con el nombre de todos los platos elegidos por el usuario.
     */
     public String getNombresPedido(){
-        
         String pedido="";
         for (Plato p: platosElegidos){  //String x="amburgueza"
             pedido+= p.getNombrePlato(); // Plato x =new Plato(String amburgueza)
         }
-        return pedido;// hambru ,papas, 
-        
-        
+        return pedido;// hambru ,papas,   
     }
-     /**
-    *param {plato} p, agregar los objetos Plato en el arraylist<plato> platosElegidos
-    *
-    */
     
+    /**
+    * Método que agrega un plato al ArrayList de platos elegidos.
+    * @param p Un plato del pedido.
+    */
     protected void agregarPlatos(Plato p){
         platosElegidos.add(p);
     }
