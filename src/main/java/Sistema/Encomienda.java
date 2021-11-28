41a4ce0d75d9959ad4bd2cc049aa1197aed0c749
melpgala <93047039+melpgala@users.noreply.github.com>
2021-11-28 11:41:38 -0500

    comentarios

--- src/main/java/Sistema/Encomienda.java
+++ src/main/java/Sistema/Encomienda.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Contiene las clases del proyecto
  */
 package Sistema;
 import Enums.tipoEncomienda;
@@ -10,20 +8,39 @@ import java.time.LocalDateTime;
 
 
 /**
- *
- * @author User
+ * Esta clase contiene los atributos y métodos de una encomienda.
+ * @author Alex Bermudez
  */
 public class Encomienda extends Servicio{
     int cantidadProductos;
     tipoEncomienda tipoE;
     
+    /**
+    * Método que devuelve la cantidad de productos de una encomienda.
+    * @return Devuelve la cantidad de productos de una encomienda.
+    */
     protected int getCantidadProductos(){
         return cantidadProductos;
     }
+    
+    /**
+    * Método que devuelve el tipo de encomienda.
+    * @return Devuelve el tipo de encomienda.
+    */
     protected tipoEncomienda getTipoE(){
         return tipoE;
     }
     
+    
+    /**
+    * Método constructor parametrizado.
+    * @tipoS Tipo de servicio.
+    * @fechaHora Fecha y hora actual en la que se realizo la encomienda.
+    * @codigo Codigo de servicio. 
+    * @cantProd Cantidad de productos de la encomienda.
+    * @tipoE Tipo de encomienda.
+    * @author Alex Bermudez
+    */
     public Encomienda(tipoServicio tipoS, LocalDateTime fechaHora, int codigo, int cantProd, tipoEncomienda tipoE){
         super(tipoS, fechaHora, codigo);       
         this.cantidadProductos = cantProd;
