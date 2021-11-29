f7c7a6f8b2ed53f764a70b91701b30d95cd4a5e9
melpgala <93047039+melpgala@users.noreply.github.com>
2021-11-28 21:51:46 -0500

    agregacion de comentarios

--- src/main/java/Sistema/Taxi.java
+++ src/main/java/Sistema/Taxi.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 /**
  * Esta clase contiene los métodos y atributos de un taxi.
  * @author Melanie Peñaherrera
- * @see Servicio
+ * @author Alex Bermudez
  */
 public class Taxi extends Servicio {
     private int numeroPasajeros;
@@ -28,12 +28,20 @@ public class Taxi extends Servicio {
     * @param fechaHora Fecha y hora en la que se pidio el servicio.
     * @param codigo Código por el servicio.
     * @param numeroPasajeros Número de pasajeros que tomaran el taxi.
+    * @author Melanie Peñaherrera
     */
     public Taxi(tipoServicio tipo, LocalDateTime fechaHora, int codigo, int numeroPasajeros) {
         super(tipo, fechaHora, codigo);
         this.numeroPasajeros = numeroPasajeros;
     }      
     
+    /**
+    * Método que devuelve una cadena con la información a agregar en los archivos text de un objeto taxi.
+    * @param cl Cliente del servicio de taxi
+    * @param co Conductor del servicio de taxi.
+    * @return Devuelve un String que indica la información a agregar en los archivos text de un objeto taxi.
+    * @author Alex Bermudez
+    */
     public String toString(Cliente cl, Conductor co){        
         return super.getCodigo()+","+cl.getNombreApellido()+","+co.getNombreApellido()+","+super.getRuta().getOrigen()+","+super.getRuta().getDestino()+","+super.getFechaHora()+","+this.getNumeroPasajeros()+","+super.getTipoPago()+","+super.getValorapagar();
     }
